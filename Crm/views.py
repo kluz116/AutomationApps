b255f27b2ee6f6d76772f739fc10b1a50db92733
kluz116 <engallanmusembya@gmail.com>
2023-12-18 14:36:00 +0300

    updates

---
+++ Crm/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+import datetime
+import json
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+
+# Create your views here.
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("getCustomers")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name='login.html', context={"login_form": form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return HttpResponseRedirect('/LegalDoc/')
